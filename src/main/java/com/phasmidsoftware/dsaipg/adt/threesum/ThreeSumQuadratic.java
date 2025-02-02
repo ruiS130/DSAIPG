4a240195efc22c27f6807f12b5f85efc8c69e4ba
Mingrui <yuan.ming@northeastern.edu>
2025-02-02 18:11:44 -0500

    Finish Assignment 2

--- src/main/java/com/phasmidsoftware/dsaipg/adt/threesum/ThreeSumQuadratic.java
+++ src/main/java/com/phasmidsoftware/dsaipg/adt/threesum/ThreeSumQuadratic.java
@@ -50,7 +50,30 @@ public class ThreeSumQuadratic implements ThreeSum {
      List<Triple> getTriples(int j) {
          List<Triple> triples = new ArrayList<>();
         // TO BE IMPLEMENTED  : for each candidate, test if a[i] + a[j] + a[k] = 0.
-throw new RuntimeException("implementation missing");
+         int target = -a[j];
+         int i = 0;
+         int k = length - 1;
+
+         while(i < j && k > j) {
+             if(a[i] + a[k] == target) {
+                 triples.add(new Triple(a[i], a[j], a[k]));
+
+                 while(i < j && a[i] == a[i+1]) {
+                     i++;
+                 }
+                 while(k > j && a[k] == a[k-1]) {
+                     k--;
+                 }
+
+                 i++;
+                 k--;
+             } else if(a[i] + a[k] < target) {
+                 i++;
+             } else {
+                 k--;
+             }
+         }
+         return triples;
     }
 
     private final int[] a;
--- src/main/java/com/phasmidsoftware/dsaipg/adt/threesum/ThreeSumQuadrithmic.java
+++ src/main/java/com/phasmidsoftware/dsaipg/adt/threesum/ThreeSumQuadrithmic.java
@@ -61,8 +61,20 @@ class ThreeSumQuadrithmic implements ThreeSum {
      * or {@code null} if no such triple can be found.
      */
     Triple getTriple(int i, int j) {
-        // TO BE IMPLEMENTED  : use binary search to find the third element
-        // END SOLUTION
+        int target = -(a[i]+a[j]);
+        int left = j + 1;
+        int right = length - 1;
+
+        while(left <= right) {
+            int mid = left+(right - left)/2;
+            if (a[mid] == target) {
+                return new Triple(a[i], a[j], a[mid]);
+            } else if (a[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
         return null;
     }
 
